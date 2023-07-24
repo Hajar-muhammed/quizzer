4827242c9b10eccab6fcc7533f402c51a6e8aa1f
Hajar-muhammed <60480715+Hajar-muhammed@users.noreply.github.com>
2023-07-24 19:02:44 +0300

    Update process.php

--- quizzer/process.php
+++ quizzer/process.php
@@ -1,39 +1,39 @@
-<?php include 'database.php';
- session_start(); 
- //check score 
- if(!isset($_SESSION['score'])){
-	 $_SESSION['score'] = 0;
- }
- if($_POST){
-	 $number = $_POST['number'];
-	 $selected_choice = $_POST['choice'];
+<?php
+include 'database.php';
+session_start();
+//check if te score is set
+if(!isset($_SESSION['score'])){
+	$_SESSION['score'] =0;
+}
+
+if($_POST){
+	$number = $_POST['number'];
+	$selected_choice=$_POST['choice'];
 	$next = $number+1;
-	
- //get totai number of questions
-$query = "SELECT * FROM `questions`";
-$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
-$total = $results->num_rows; 
- 
- //get correct choice 
- $query = "SELECT * FROM `choices` 
-              WHERE question_number = $number AND is_correct = 1";
-			  //excute the query
-$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
-//convert result to array 
-$row = $result->fetch_assoc();
-//set correct choice 
-$correct_choice = $row['id'];
-//add to scores
-if($correct_choice == $selected_choice){
-  $_SESSION['score']+1;
-}  
-if($number == $total){
-	header("location: final.php");
-	exit();
-}else{
-	header("location: question.php?n=".$next);
+	//get total number of question
+    $query ="SELECT * FROM questions";
+    $result =$mysqli->query($query)or die ($mysqli->error.__LINE__);
+    $total = $result->num_rows;
+
+
+	//selecting the correct choise from database
+	$query = "SELECT * FROM `choices`
+	             WHERE question_number = $number AND is_correct=1";
+	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+	$row = $result->fetch_assoc();
+	//set correct choise 
+	$correct_choice = $row['id'];
+	//compare
+	if ($correct_choice == $selected_choice) {
+		//answer is correct  & we have increase the score
+		$_SESSION['score']=$_SESSION['score']+1;
+	}
+	//checl if last question
+	if($number == $total){
+		header("location: final.php");
+		exit();
+	}else{
+		header("location: question.php?n=".$next);
+	}
 }
-}
-
-
